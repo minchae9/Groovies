5642c65a2a1ed551906e051f7e2ab492f803ca00
une9 <59572449+une9@users.noreply.github.com>
2021-11-26 01:09:09 +0900

    끝!

--- drf_server/accounts/models.py
+++ drf_server/accounts/models.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    nickname = models.CharField(max_length=20)
+    nickname = models.CharField(max_length=100)
     profile_path = models.IntegerField(default=0)
